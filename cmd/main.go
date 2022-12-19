c70daf1bbbf1355edcfa327e043575183da9cb66
Anatole <me@anatole.dev>
2022-12-19 23:56:36 +0100

    try kill process in prestop hook

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"runtime"
 	"runtime/debug"
 	"strconv"
+	"syscall"
 	"time"
 
 	"github.com/bugsnag/panicwrap"
@@ -60,7 +61,7 @@ func main() {
 	zap.S().Debugf("MaxProcs: ", runtime.GOMAXPROCS(0))
 
 	sig := make(chan os.Signal, 1)
-	signal.Notify(sig)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGILL, syscall.SIGTERM, syscall.SIGQUIT)
 
 	c, cancel := context.WithCancel(context.Background())
 
