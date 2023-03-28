04bd6259819a08b7fb26658460b15b0dffd37f57
Anatole <me@anatole.dev>
2023-03-28 20:21:59 +0200

    fix: eager subscribe

--- internal/app/connection/websocket/websocket_read.go
+++ internal/app/connection/websocket/websocket_read.go
@@ -41,6 +41,15 @@ func (w *WebSocket) Read(gctx global.Context) {
 			return
 		}
 
+		// Subscribe to whispers
+		if _, _, err := w.evm.Subscribe(gctx, w.ctx, events.EventTypeWhisper, events.EventCondition{
+			"session_id": w.SessionID(),
+		}, client.EventSubscriptionProperties{
+			Auto: true,
+		}); err != nil {
+			zap.S().Errorw("whisper subscription error", "error", err, "session_id", w.SessionID())
+		}
+
 		defer func() {
 			deferred = true
 
@@ -123,15 +132,6 @@ func (w *WebSocket) Read(gctx global.Context) {
 		s   *string
 		err error
 	)
-
-	// Subscribe to whispers
-	if _, _, err = w.evm.Subscribe(gctx, w.ctx, events.EventTypeWhisper, events.EventCondition{
-		"session_id": w.SessionID(),
-	}, client.EventSubscriptionProperties{
-		Auto: true,
-	}); err != nil {
-		zap.S().Errorw("whisper subscription error", "error", err, "session_id", w.SessionID())
-	}
 
 	for {
 		select {
